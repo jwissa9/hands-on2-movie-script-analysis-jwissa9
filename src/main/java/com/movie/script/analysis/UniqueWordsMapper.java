e6ca90ef6f3bee695a61eda96030dd8dcc5a3901
jwissa9 <59879274+jwissa9@users.noreply.github.com>
2024-09-19 20:38:56 +0000

    Completed Movie Script Analysis Assignment with a 3rd Task

--- src/main/java/com/movie/script/analysis/UniqueWordsMapper.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsMapper.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class UniqueWordsMapper extends Mapper<Object, Text, Text, Text> {
@@ -14,6 +15,31 @@ public class UniqueWordsMapper extends Mapper<Object, Text, Text, Text> {
 
     @Override
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] tokens = line.split(":", 2); //split the character and their quote
+        if (tokens.length == 2) {
+            String name = tokens[0].trim(); //character name
+            String quote = tokens[1].trim(); //quote
+
+            //get the words from the quote, split by whitespace
+            String[] words = quote.split("\\s+");
+            //create a new hash set the stores the unique words
+            Set<String> uniqueWords = new HashSet<>();
+
+            //put the words in the hash set. the hash set ensures only unique words, no duplicates
+            for (String i : words) {
+                uniqueWords.add(i.toLowerCase());
+            }
+
+            //set the name as the key and the word as the value
+            //set each unique word as the key for each character
+            character.set(name);
+            for (String u : uniqueWords) {
+                word.set(u);
+                context.write(character, word);
+            }
+        }
 
     }
+
 }
--- src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
@@ -5,11 +5,23 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Set;
 
 public class UniqueWordsReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Set<String> unique = new HashSet<>();
 
+        //gets the values associated from the key
+        //hashset ensures uniqueness
+        for (Text v : values) {
+            unique.add(v.toString());
+        }
+
+        //character's name (key) and their unique word (value) are written to the output from the hash set
+        for (String u : unique) {
+            context.write(key, new Text(u));
+        }
     }
 }
